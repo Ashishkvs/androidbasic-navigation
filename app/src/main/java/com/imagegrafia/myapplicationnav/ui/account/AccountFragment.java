79d2c0a7c0e0feb0a2acf145dd5da04dbce41505
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2021-11-02 12:52:51 +0530

    initial commit

---
+++ app/src/main/java/com/imagegrafia/myapplicationnav/ui/account/AccountFragment.java
@@ -0,0 +1,66 @@
+package com.imagegrafia.myapplicationnav.ui.account;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.imagegrafia.myapplicationnav.databinding.FragmentAccountBinding;
+import com.imagegrafia.myapplicationnav.databinding.FragmentGalleryBinding;
+
+public class AccountFragment extends Fragment {
+
+//    private AccountViewModel mViewModel;
+//
+//    public static AccountFragment newInstance() {
+//        return new AccountFragment();
+//    }
+
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+//                             @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.account_fragment, container, false);
+//    }
+
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//        mViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+//        // TODO: Use the ViewModel
+//    }
+    private AccountViewModel accountViewModel;
+    private FragmentAccountBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        accountViewModel =
+                new ViewModelProvider(this).get(AccountViewModel.class);
+
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textAccount;
+        accountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
